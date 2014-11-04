5b74183948d0e7e3e84a3adfea53bbb532a18b7d
Steve Bischoff <bischofs13@gmail.com>
2014-11-04 16:46:26 -0500

    second change

---
+++ DataTools/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from tastypie.utils.timezone import now
+
+
+# Create your models here.
+
+
--- TestSite/urls.py
+++ TestSite/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from TestSite.api import EntryResource
+from TestSite import views
 
-entry_resource = EntryResource()
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'TestSite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    (r'^api/', include(entry_resource.urls)),
-    url(r'^admin/', include(admin.site.urls)),
+        url(r'^admin/', include(admin.site.urls)),
+        url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
