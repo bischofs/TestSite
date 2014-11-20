894384c28ca8f5eeb36cef1127621447fc336dc8
Steve Bischoff <bischofs13@gmail.com>
2014-11-20 17:01:51 -0500

    removed UserProfile model

--- DataTools/models.py
+++ DataTools/models.py
@@ -5,21 +5,6 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save, pre_delete
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, primary_key=True, related_name='profile')
-    
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __unicode__(self):
-        return self.user.username
-        
-    @receiver(post_save, sender=User)
-    def create_profile_for_user(sender, instance=None, created=False, **kwargs):
-        if created:
-            UserProfile.objects.get_or_create(user=instance)
-                
-    
 
 
 # Create your models here.
--- DataTools/serializers.py
+++ DataTools/serializers.py
@@ -10,47 +10,12 @@ from django.http  import HttpResponse as request
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     #posts = serializers.HyperlinkedIdentityField('posts', view_name='userpost-list', lookup_field='username')
-    context={'request': request}    
 
 
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'is_staff' )
+        context={'request': request}
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ('url', 'name')
 
-
-class UserProfileSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(source='pk', read_only=True)
-    username = serializers.CharField(source='user.username', read_only=True)
-    email = serializers.CharField(source='user.email', required=False)
-    first_name = serializers.CharField(source='user.first_name', required=False)
-    last_name = serializers.CharField(source='user.last_name', required=False)
-
-    class Meta:
-        model = UserProfile
-        fields = (
-            'id', 'username', 'email', 'first_name', 'last_name', 'tagline',
-            'created_at', 'updated_at',
-        )
-        read_only_fields = ('created_at', 'updated_at',)
-
-    def restore_object(self, attrs, instance=None):
-        profile = super(UserProfileSerializer, self).restore_object(
-            attrs, instance
-        )
-
-        if profile:
-            user = profile.user
-
-            user.email = attrs.get('user.email', user.email)
-            user.first_name = attrs.get('user.first_name', user.first_name)
-            user.last_name = attrs.get('user.last_name', user.last_name)
-
-            user.save()
-
-        return profile
