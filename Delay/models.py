3cf0ccf0dfad7e89da1ed2c25bd279350aac4d42
Steve Bischoff <bischofs13@gmail.com>
2015-06-16 17:01:11 -0400

    front end delay work

--- Delay/models.py
+++ Delay/models.py
@@ -14,35 +14,32 @@ class DelayPrep:
                 self.mapDict = mapDict
                 self.coHigh = coHigh
 
-                self.delaySpecies = pd.DataFrame.from_items([('Air_Flow_Rate', self.data[self.mapDict['Air_Flow_Rate']]),('Nitrogen_X_Dry',self.data[self.mapDict['Nitrogen_X_Dry']]),
-                ('Total_Hydrocarbons_Wet',self.data[self.mapDict['Total_Hydrocarbons_Wet']]), 
-                ('Methane_Wet',self.data[self.mapDict['Methane_Wet']]),('Oxygen_Dry',self.data[self.mapDict['Oxygen_Dry']]), ('Nitrogen_Monoxide_Dry',self.data[self.mapDict['Nitrogen_Monoxide_Dry']]),
-                ('Carbon_Dioxide_Dry',self.data[self.mapDict['Carbon_Dioxide_Dry']]),('Engine_Torque',self.data[self.mapDict['Engine_Torque']])])
-
-
+                self.delaySpecies = pd.DataFrame.from_items([('Air_Flow_Rate', self.data[self.mapDict['Air_Flow_Rate']]),
+                                                             ('Nitrogen_X_Dry',self.data[self.mapDict['Nitrogen_X_Dry']]),
+                                                             ('Total_Hydrocarbons_Wet',self.data[self.mapDict['Total_Hydrocarbons_Wet']]), 
+                                                             ('Methane_Wet',self.data[self.mapDict['Methane_Wet']]),
+                                                             ('Oxygen_Dry',self.data[self.mapDict['Oxygen_Dry']]),
+                                                             ('Nitrogen_Monoxide_Dry',self.data[self.mapDict['Nitrogen_Monoxide_Dry']]),
+                                                             ('Carbon_Dioxide_Dry',self.data[self.mapDict['Carbon_Dioxide_Dry']]),
+                                                             ('Engine_Torque',self.data[self.mapDict['Engine_Torque']])])
+                
                 if(self.coHigh == False):
                         self.delaySpecies['Carbon_Monoxide_Low_Dry'] = self.data[self.mapDict['Carbon_Monoxide_Low_Dry']]
                 else:
                         self.delaySpecies['Carbon_Monoxide_High_Dry'] = self.data[self.mapDict['Carbon_Monoxide_High_Dry']]
-
-
-                
-
                 
 
         def create_windows(self):
-
+                
                 pctChange = self.data[self.mapDict['Engine_Torque']].diff()
                 largest = pctChange.nlargest(3)
-                                
+                
                 low = largest.index[1] - 100
                 high = largest.index[1] + 100                
-
+                
                 #torq = pd.Series(torqWindow, name="Torque") 
-
-
-#                import ipdb; ipdb.set_trace()
-
+                #import ipdb; ipdb.set_trace()
+                
                 self.delaySpecies = self.delaySpecies[low:high]
 
                 self.delaySpecies = (self.delaySpecies - self.delaySpecies.mean()) / (self.delaySpecies.max() - self.delaySpecies.min())
@@ -51,7 +48,6 @@ class DelayPrep:
                 self.js = self.delaySpecies.to_json()
 
                 return self.js
-
 
                 #jsonDict = { "torque": torqWindow}
 
--- static/javascripts/delay/delay.controller.js
+++ static/javascripts/delay/delay.controller.js
@@ -11,124 +11,125 @@
      */
     function DelayController($scope, $http) {
 
+
+        $scope.delay = 0;
+
         $scope.nox = function() {
-
             $scope.labels = _.keys($scope.spec.Nitrogen_X_Dry);
-            $scope.series = ['Species', 'Torque'];
+            $scope.series = ['NOx', 'Torque'];
             $scope.data = [_.values($scope.spec.Nitrogen_X_Dry), _.values($scope.spec.Engine_Torque)];
-
         };
         $scope.ch4 = function() {
-
-            $scope.labels = _.keys($scope.spec.Methane_Wet);
-            $scope.series = ['Species', 'Torque'];
-            $scope.data = [_.values($scope.spec.Methane_Wet), _.values($scope.spec.Engine_Torque)];
-
+            $scope.labels = _.keys($scope.spec.Methane_Wet).splice(50, 100);
+            $scope.series = ['CH4', 'Torque'];
+            $scope.data = [_.values($scope.spec.Methane_Wet).splice(50, 100), _.values($scope.spec.Engine_Torque).splice(50, 100)];
         };
         $scope.co = function() {
-
             $scope.labels = _.keys($scope.spec.Carbon_Monoxide_High_Dry);
-            $scope.series = ['Species', 'Torque'];
+            $scope.series = ['CO', 'Torque'];
             $scope.data = [_.values($scope.spec.Carbon_Monoxide_High_Dry), _.values($scope.spec.Engine_Torque)];
-
         };
         $scope.co2 = function() {
-
             $scope.labels = _.keys($scope.spec.Carbon_Dioxide_Dry);
-            $scope.series = ['Species', 'Torque'];
+            $scope.series = ['CO2', 'Torque'];
             $scope.data = [_.values($scope.spec.Carbon_Dioxide_Dry), _.values($scope.spec.Engine_Torque)];
-
         };
         $scope.o2 = function() {
-
             $scope.labels = _.keys($scope.spec.Oxygen_Dry);
-            $scope.series = ['Species', 'Torque'];
+            $scope.series = ['O2', 'Torque'];
             $scope.data = [_.values($scope.spec.Oxygen_Dry), _.values($scope.spec.Engine_Torque)];
-
         };
         $scope.no = function() {
-
             $scope.labels = _.keys($scope.spec.Nitrogen_Monoxide_Dry);
-            $scope.series = ['Species', 'Torque'];
+            $scope.series = ['NO', 'Torque'];
             $scope.data = [_.values($scope.spec.Nitrogen_Monoxide_Dry), _.values($scope.spec.Engine_Torque)];
-
         };
-
         $scope.maf = function() {
-
             $scope.labels = _.keys($scope.spec.Air_Flow_Rate);
-            $scope.series = ['Species', 'Torque'];
+            $scope.series = ['MFRAIR', 'Torque'];
             $scope.data = [_.values($scope.spec.Air_Flow_Rate), _.values($scope.spec.Engine_Torque)];
-
         };
         $scope.thc = function() {
-
             $scope.labels = _.keys($scope.spec.Total_Hydrocarbons_Wet);
-            $scope.series = ['Species', 'Torque'];
+            $scope.series = ['THC', 'Torque'];
             $scope.data = [_.values($scope.spec.Total_Hydrocarbons_Wet), _.values($scope.spec.Engine_Torque)];
-
         };
 
-     $scope.forward = function() {
+        $scope.left = function() {
 
+
+            var right = _.values($scope.spec.Methane_Wet).splice((150 + $scope.delay), 50);
+            var left = _.values($scope.spec.Methane_Wet).splice(0, (50 + $scope.delay));
+            var data = _.values($scope.spec.Methane_Wet).splice((50 + $scope.delay), 100);
+
+            $scope.delay++;
+
+            var item = data.shift();
+            left.push(item)
+            var item = right.shift();
+            data.push(item);
 
             $scope.series = ['Species', 'Torque'];
-
+            $scope.data = [data, _.values($scope.spec.Engine_Torque).splice(50, 100)];
+            console.log($scope.delay);
 
         };
-     $scope.back = function() {
+        $scope.right = function() {
 
+            var right = _.values($scope.spec.Methane_Wet).splice((150 + $scope.delay), 50);
+            var left = _.values($scope.spec.Methane_Wet).splice(0, (50 + $scope.delay));
+            var data = _.values($scope.spec.Methane_Wet).splice((50 + $scope.delay), 100);
+
+            $scope.delay--;
+
+            var item = data.pop();
+            right.unshift(item);
+            var item = left.pop();
+            data.unshift(item);
 
             $scope.series = ['Species', 'Torque'];
-
+            $scope.data = [data, _.values($scope.spec.Engine_Torque).splice(50, 100)];
+            console.log($scope.delay);
 
         };
 
 
 
-
-        $scope.read = function() {
-
+        var read = function() {
             $http.get('/api/v1/data/delay/')
+                .success(function(data) {
+                    var spec = JSON.parse(data)
+                    $scope.spec = spec
+                    $scope.options = {
+                        pointDot: false,
+                        showTooltips: false
+                    };
+                    $scope.series = ['Species', 'Torque'];
+                    createWindow();
 
-            .success(function(data) {
-                var spec = JSON.parse(data)
-                $scope.spec = spec
-                $scope.options = {
-                    pointDot: false,
-                    showTooltips: false
-                };
-                $scope.labels = _.keys(spec.Methane_Wet);
-                $scope.series = ['Species', 'Torque'];
-                $scope.data = [_.values(spec.Methane_Wet), _.values(spec.Engine_Torque)];
-
-            })
-
-            .error(function(data, status, headers, config) {
-
-                throw new Error('Something went wrong with reading data');
-
-            });
+                })
+                .error(function(data, status, headers, config) {
+                    throw new Error('Something went wrong with reading data');
+                });
 
         };
 
-        $scope.read();
 
-        // $scope.labels = [12,123,123,123,123,123,123,123,123]//_.keys(spec.Oxygen_Dry)
-        // $scope.series = ['Species'];
-        // $scope.data = [123,123,123,123,123,1231,2312,3123,123]//_.values(spec.Oxygen_Dry)
-
-
-        // $scope.onClick = function(points, evt) {
-        //     console.log(points, evt);
-        // };
-        // var randomScalingFactor = function(scale) {
-        //     return Math.round(Math.random() * scale)
-        // };
+        var createWindow = function() {
 
 
 
 
+
+
+
+
+
+        };
+
+
+
+        read();
 
 
     }
